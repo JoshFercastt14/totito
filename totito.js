e46467ad0522330465c11659302a0c4bfcb2800d
Fernando Juarez <124473194+JoshFercastt14@users.noreply.github.com>
2023-05-22 22:27:52 -0600

    Add files via upload

---
+++ totito.js
@@ -0,0 +1,144 @@
+function imagen(){ 
+    document.getElementById('uno').src="cir.jpg";
+    document.getElementById('uno').style.height="90px";
+    document.getElementById('uno').style.width="90px";
+    document.getElementById('dos').style.display="none";
+    
+        // body...
+    
+    }
+    function img(){ 
+    document.getElementById('dos').src="x.jpg";
+    document.getElementById('dos').style.height="70px";
+    document.getElementById('dos').style.width="70px";
+    document.getElementById('uno').style.display="none";
+        // body...
+    }
+    function im(){ 
+    document.getElementById('tres').src="cir.jpg";
+    document.getElementById('tres').style.height="90px";
+    document.getElementById('tres').style.width="90px";
+    document.getElementById('cuatro').style.display="none";
+    
+        // body...
+    
+    }
+    function ij(){ 
+    document.getElementById('cuatro').src="x.jpg";
+    document.getElementById('cuatro').style.height="70px";
+    document.getElementById('cuatro').style.width="70px";
+    document.getElementById('tres').style.display="none";
+        // body...
+    }
+    function mage(){ 
+    document.getElementById('cinco').src="cir.jpg";
+    document.getElementById('cinco').style.height="90px";
+    document.getElementById('cinco').style.width="90px";
+    document.getElementById('seis').style.display="none";
+    
+        // body...
+    
+    }
+    function kl(){ 
+    document.getElementById('seis').src="x.jpg";
+    document.getElementById('seis').style.height="70px";
+    document.getElementById('seis').style.width="70px";
+    document.getElementById('cinco').style.display="none";
+        // body...
+    }
+    function lolo(){ 
+    document.getElementById('siete').src="cir.jpg";
+    document.getElementById('siete').style.height="90px";
+    document.getElementById('siete').style.width="90px";
+    document.getElementById('ocho').style.display="none";
+    
+        // body...
+    
+    }
+    function lola(){ 
+    document.getElementById('ocho').src="x.jpg";
+    document.getElementById('ocho').style.height="70px";
+    document.getElementById('ocho').style.width="70px";
+    document.getElementById('siete').style.display="none";
+        // body...
+    }
+    function mio(){ 
+    document.getElementById('nueve').src="cir.jpg";
+    document.getElementById('nueve').style.height="90px";
+    document.getElementById('nueve').style.width="90px";
+    document.getElementById('dies').style.display="none";
+    
+        // body...
+    
+    }
+    function mi(){ 
+    document.getElementById('dies').src="x.jpg";
+    document.getElementById('dies').style.height="70px";
+    document.getElementById('dies').style.width="70px";
+    document.getElementById('nueve').style.display="none";
+        // body...
+    }
+    function lilo(){ 
+    document.getElementById('once').src="cir.jpg";
+    document.getElementById('once').style.height="90px";
+    document.getElementById('once').style.width="90px";
+    document.getElementById('dose').style.display="none";
+    
+        // body...
+    
+    }
+    function lila(){ 
+    document.getElementById('dose').src="x.jpg";
+    document.getElementById('dose').style.height="70px";
+    document.getElementById('dose').style.width="70px";
+    document.getElementById('once').style.display="none";
+        // body...
+    }
+    function g(){ 
+    document.getElementById('a').src="cir.jpg";
+    document.getElementById('a').style.height="90px";
+    document.getElementById('a').style.width="90px";
+    document.getElementById('b').style.display="none";
+    
+        // body...
+    
+    }
+    function h(){ 
+    document.getElementById('b').src="x.jpg";
+    document.getElementById('b').style.height="70px";
+    document.getElementById('b').style.width="70px";
+    document.getElementById('a').style.display="none";
+        // body...
+    }
+    function i(){ 
+    document.getElementById('c').src="cir.jpg";
+    document.getElementById('c').style.height="90px";
+    document.getElementById('c').style.width="90px";
+    document.getElementById('d').style.display="none";
+    
+        // body...
+    
+    }
+    function j(){ 
+    document.getElementById('d').src="x.jpg";
+    document.getElementById('d').style.height="70px";
+    document.getElementById('d').style.width="70px";
+    document.getElementById('c').style.display="none";
+        // body...
+    }
+    function k(){ 
+    document.getElementById('e').src="cir.jpg";
+    document.getElementById('e').style.height="90px";
+    document.getElementById('e').style.width="90px";
+    document.getElementById('f').style.display="none";
+    
+        // body...
+    
+    }
+    function l(){ 
+    document.getElementById('f').src="x.jpg";
+    document.getElementById('f').style.height="70px";
+    document.getElementById('f').style.width="70px";
+    document.getElementById('e').style.display="none";
+        // body...
+    }
